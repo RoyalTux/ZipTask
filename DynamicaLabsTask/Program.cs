b55c917302cc4db7578c53e220be93244cd003c0
Dmitriy <dmitry.shebetovsky@gmail.com>
2019-06-15 01:12:28 +0300

    Extract .csv files

--- DynamicaLabsTask/Program.cs
+++ DynamicaLabsTask/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,33 @@ namespace DynamicaLabsTask
     {
         static void Main(string[] args)
         {
+            ExtractCsvFiles();
+        }
+
+        public static void ExtractCsvFiles()
+        {
+            //Console.Write("Insert zip archive path here: ");
+            //string pathToZip = Console.ReadLine();
+            //Console.WriteLine("Provide path where to extract the zip file: ");
+            //string extractPath = Console.ReadLine();
+            string pathToZip = @"C:\Users\Dmitriy\Desktop\DynamicaLabsTask.zip";
+            string extractPath = @"C:\Users\Dmitriy\Desktop\test";
+            extractPath = Path.GetFullPath(extractPath);
+            if (!extractPath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+                extractPath += Path.DirectorySeparatorChar;
+
+            using (ZipArchive archive = ZipFile.OpenRead(pathToZip))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (entry.FullName.EndsWith(".csv", StringComparison.OrdinalIgnoreCase))
+                    {
+                        string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.FullName));
+                        if (destinationPath.StartsWith(extractPath, StringComparison.Ordinal))
+                            entry.ExtractToFile(destinationPath);
+                    }
+                }
+            }
         }
     }
 }
